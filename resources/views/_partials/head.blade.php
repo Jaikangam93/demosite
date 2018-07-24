1cb37455b6c93ecff8328601ed5731f60f28be25
Jaikangam Malangmei <jaikangam12@gmail.com>
2018-07-24 09:51:33 +0530

    mdoified

--- resources/views/_partials/head.blade.php
+++ resources/views/_partials/head.blade.php
@@ -1,27 +1,32 @@
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 	<title>{{ config('app.name', 'ABC School ') }}</title>
 
 	
 
-	<!-- Global stylesheets -->
+	<!-- Global stylesheets by default index  -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
 	<link href="{{ asset('assets/css/icons/icomoon/styles.css') }}" rel="stylesheet" type="text/css">
 	<link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css">
 	<link href="{{ asset('assets/css/core.css') }}" rel="stylesheet" type="text/css">
 	<link href="{{ asset('assets/css/components.css') }}" rel="stylesheet" type="text/css">
 	<link href="{{ asset('assets/css/colors.css') }}" rel="stylesheet" type="text/css">
-	<!-- /global stylesheets -->
+	<!-- /global stylesheets by default Index  -->
 
-	<!-- Core JS files -->
+
+
+
+
+	<!-- Core JS files by default Index  -->
 	<script type="text/javascript" src="{{ asset('assets/js/plugins/loaders/pace.min.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('assets/js/core/libraries/jquery.min.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('assets/js/core/libraries/bootstrap.min.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('assets/js/plugins/loaders/blockui.min.js') }}"></script>
-	<!-- /core JS files -->
+	<!-- /core JS files by default Index -->
 
-	<!-- Theme JS files -->
+	<!-- Theme JS files (By Default Index )-->
 	<script type="text/javascript" src="{{ asset('assets/js/plugins/visualization/d3/d3.min.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('assets/js/plugins/visualization/d3/d3_tooltip.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('assets/js/plugins/forms/styling/switchery.min.js') }}"></script>
@@ -34,4 +39,4 @@
 	<script type="text/javascript" src="{{ asset('assets/js/pages/dashboard.js') }}"></script>
 
 	<script type="text/javascript" src="{{ asset('assets/js/plugins/ui/ripple.min.js') }}"></script>
-	<!-- /theme JS files -->
+	<!-- /theme JS files (By Default Index ) -->
